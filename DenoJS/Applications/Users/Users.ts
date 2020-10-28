97096674bce98fb5523a9cbfb4e1b83172304d3c
Bored Panda <boredpanda@Boreds-Mac.local>
2020-10-29 01:26:22 +0300

    --- Auto-Git Commit ---

--- DenoJS/Applications/Users/Users.ts
+++ DenoJS/Applications/Users/Users.ts
@@ -6,20 +6,21 @@ export default class Users{
     private readonly db_user: string = 'users'
 
     constructor(context: Record<string, any>, next: Function, router: Router) {
-        router.post("/Users/Register", (context, next) => { this.signUp(context, next) }) 
+        router.post("/Users/Register", async (context, next) => { await this.signUp(context, next) }) 
 
 
        context.response.body = "Hello world Users!";
     }
 
     async signUp(context: Record<string, any>, next: Function): Promise<void>{
-        let body = await context.request.body();
+        try {
+           
 
-        
+            context.response.body = "register";
 
-
-        console.log(body)
-        context.response.body = "register";
+        } catch (error) {
+            console.log(error.message);
+        }
     }
 
     signIn(){
