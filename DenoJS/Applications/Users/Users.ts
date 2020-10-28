6bcddff224d84b5d4a5d07aaee567e84ebf3f266
Bored Panda <boredpanda@Boreds-Mac.local>
2020-10-29 00:26:02 +0300

    --- Auto-Git Commit ---

--- DenoJS/Applications/Users/Users.ts
+++ DenoJS/Applications/Users/Users.ts
@@ -6,14 +6,14 @@ export default class Users{
     private readonly db_user: string = 'users'
 
     constructor(context: any, next: Function, router: Router) {
-        router.get("/Users/Register", (context, next) => { this.signUp(context, next) }) 
+        router.post("/Users/Register", (context, next) => { this.signUp(context, next) }) 
 
 
        context.response.body = "Hello world Users!";
     }
 
     signUp(context: any, next: any): void{
-        //let mail: string = 
+        
         console.log(context.params)
         context.response.body = "register";
     }
