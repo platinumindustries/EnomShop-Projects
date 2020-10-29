2dc7bd062ccb36af50fc4a01319b7280ae321a1f
Bored Panda <boredpanda@Boreds-Mac.local>
2020-10-29 14:11:23 +0300

    --- Auto-Git Commit ---

--- DenoJS/Applications/Users/Users.ts
+++ DenoJS/Applications/Users/Users.ts
@@ -1,9 +1,11 @@
 import { Router } from "https://deno.land/x/oak/mod.ts"
 
 export default class Users{
+    private readonly domain: string = '127.0.0.1'
+    private readonly port: number = 5984
     private readonly db_name: string = 'users'
-
-    private readonly db_user: string = 'users'
+    private readonly db_user: string = 'admin'
+    private readonly db_pass: string = 'Macho20!2'
 
     constructor(context: Record<string, any>, next: Function, router: Router) {
         router.post("/Users/Register", async (context, next) => { await this.signUp(context, next) }) 
@@ -16,13 +18,19 @@ export default class Users{
             let body = context.request.body(),
                 email = (await body.value).get("e-mail")
 
-            fetch(url, {method: 'GET'})
+            let x = await fetch(`http://${this.domain}:${this.port}/${this.db_name}`, {method: 'GET'})
+
+            console.log(x)
 
             //create an account with the email adress and force user to activate via email
-            context.response.body = name
+            context.response.body = 'name'
     }
 
     signIn(){
+
+    }
+
+    setup(){
 
     }
 }
