798740ff39a3bd63f4058de880a3533ebfa37037
Bored Panda <boredpanda@Boreds-Mac.local>
2020-11-23 01:04:51 +0300

    --- Auto-Git Commit ---

--- DenoJS/System/Library/CouchDb.ts
+++ DenoJS/System/Library/CouchDb.ts
@@ -1,8 +1,8 @@
 export default class CouchDb{
     private static readonly domain: string = '127.0.0.1'
-    private static readonly port: number = 5984
-    private static readonly username: string = 'admin'
-    private static readonly password: string = 'Macho20!2'
+    private static readonly port: number = 8091s
+    private static readonly username: string = 'Administrator'
+    private static readonly password: string = 'Macho2012'
 
     constructor() {
     }
