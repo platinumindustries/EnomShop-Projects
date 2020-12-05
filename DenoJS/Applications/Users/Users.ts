d687f3a5f95b4244b848b36ea407511da24b2270
Bored Panda <boredpanda@Boreds-Mac.local>
2020-12-05 18:07:14 +0300

    --- Auto-Git Commit ---

--- DenoJS/Applications/Users/Users.ts
+++ DenoJS/Applications/Users/Users.ts
@@ -16,17 +16,27 @@ export default class Users{
                 url = 'http://localhost:8091/settings/rbac/users/local/' + data.mail
             
                 delete data.mail
-                data.roles = 'basic'
 
             let res = await fetch(url, { method: 'GET', headers: { 'Authorization': 'Basic ' + Users.Cert } })   
                 if (res.status === 200) { //USERS#3000 - username already exists
                      throw Object.assign({ '_code': 'USERS#3000' }, res)
                 }
 
-                //res = await fetch(url, { method: 'PUT', headers: { 'Authorization': 'Basic ' + Users.Cert, 'Content-Type': 'application/json' }, mode: "cors", body: JSON.stringify(data) })
-                //console.log(res, await res.json())
-                res = await fetch('https://postman-echo.com/put', { method: 'PUT', headers: { 'Authorization': 'Basic ' + Users.Cert, 'Content-Type': 'application/json' }, mode: "cors", body: JSON.stringify(data) })
+                res = await fetch(url, { method: 'PUT', headers: { 'Authorization': 'Basic ' + Users.Cert, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' }, mode: "cors", body: JSON.stringify(data) })
                 console.log(res, await res.json())
+
+
+
+
+
+
+/*
+                const _formData = new FormData();
+                Object.keys(data).forEach(key => _formData.append(key, data[key]));
+
+
+                res = await fetch('https://postman-echo.com/put', { method: 'PUT', headers: { 'Authorization': 'Basic ' + Users.Cert }, body: _formData })
+                console.log(res, await res.json())*/
 
 
             context.response.body = 'll'
