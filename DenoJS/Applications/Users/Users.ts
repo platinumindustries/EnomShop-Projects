95cae170c8bc7efcbe50bff3f84fe8a04c9d3382
Bored Panda <boredpanda@Boreds-Mac.local>
2020-12-24 00:26:32 +0300

    --- Auto-Git Commit ---

--- DenoJS/Applications/Users/Users.ts
+++ DenoJS/Applications/Users/Users.ts
@@ -2,6 +2,7 @@ import { Router } from "https://deno.land/x/oak/mod.ts"
 
 export default class Users{
     private static readonly Cert: string = btoa('Administrator' + ":" + 'Macho2012')
+    private static readonly BaseUrl: string = 'http://localhost:8091/'
 
     constructor(context: Record<string, any>, next: Function, router: Router) {
         router.post("/users/create-account", async (context, next) => { await this.createAccount(context, next) }) 
@@ -14,7 +15,7 @@ export default class Users{
             let body = await context.request.body({ type: 'form-data'}), 
                 formData = await body.value.read(), 
                 data = formData.fields, 
-                url = 'http://localhost:8091/settings/rbac/users/local/' + data.mail
+                url = Users.BaseUrl + 'settings/rbac/users/local/' + data.mail
             
                 delete data.mail
 
@@ -38,7 +39,7 @@ export default class Users{
             let body = await context.request.body({ type: 'form-data'}), 
                 formData = await body.value.read(), 
                 data = formData.fields, 
-                url = 'http://localhost:8091/settings/rbac/users/local/' + data.mail //enforce username validity
+                url = Users.BaseUrl + 'settings/rbac/users/local/' + data.mail //enforce username validity
            
             let res = await fetch(url, { method: 'DELETE', headers: { 'Authorization': 'Basic ' + Users.Cert } })   
                 if (res.status === 200){ context.response.status = 200; context.response.body = { 'msg': 'user deleted!' }; return; }
@@ -57,5 +58,26 @@ export default class Users{
 
     setup(){
 
+    }
+
+    private static async createProfileStore(){
+        try{
+            let url = Users.BaseUrl + 'pools/default/buckets' 
+            
+                delete data.mail
+
+            let res = await fetch(url, { method: 'GET', headers: { 'Authorization': 'Basic ' + Users.Cert } })   
+                if (res.status === 200){ context.response.status = 409; context.response.body = { 'msg': 'user already exists!' }; return; }
+                if (res.status === 401){ context.response.status = 401; context.response.body = { 'msg': 'invalid app certificate ' }; return; }
+                
+                res = await fetch(url, { method: 'PUT', headers: { 'Authorization': 'Basic ' + Users.Cert, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' }, body: new URLSearchParams(data).toString() })
+                    if (res.status === 401){ context.response.status = 401; context.response.body = { 'msg': 'invalid app certificate ' }; return; }
+                    if (res.status === 400){ context.response.status = 400; context.response.body = { 'msg': await res.json() }; return; } //simplify this into a simple sentence
+                    if (res.status === 200){ context.response.status = 201; context.response.body = { 'msg': 'user created' }; return; }
+                    
+                context.response.status = 502; context.response.body = { 'msg': 'undocumented response' }; return;
+        } catch(e){
+            context.response.status = 500; context.response.body = { 'type': e.name, 'msg': e.message }
+        } 
     }
 }
